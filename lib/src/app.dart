2331ca7b1255f83c18fa6554453d75cc559fd575
kinomi7 <miyuu.niko7@gmail.com>
2023-08-20 15:51:56 +0900

    add:MyApp(navigation)

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,54 @@
 //ナビゲーションの実装など
+import 'package:flutter/material.dart';
+
+import 'screens/account.dart';
+import 'screens/bookmark.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  static const _screens = [BookmarkScreen(), AccountScreen()];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'お気に入り'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'アカウント')
+          ],
+          type: BottomNavigationBarType.fixed,
+        ));
+  }
+}
